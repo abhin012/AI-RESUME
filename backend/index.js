566453459e5ecb4a2fb1868154fd16a00e9a5a6c
abhin012 <abhing03@gmail.com>
2026-06-06 16:09:51 +0530

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ const resumeRoute = require("./routes/resume");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://ai-resume-phi-orpin.vercel.app", "http://localhost:5173"]
+}));
 app.use(express.json());
 
 app.use("/api/resume", resumeRoute);
 
-mongoose.connect("mongodb://127.0.0.1:27017/resumeanalyser")
+mongoose.connect("MONGO_URI=mongodb+srv://admin:admin1234@cluster0.rprss.mongodb.net/resumeanalyser?appName=Cluster0")
   .then(() => {
     console.log("MongoDB connected");
     app.listen(process.env.PORT, () => {
