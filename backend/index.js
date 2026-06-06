82a34c137fac37d4854c861257520e1153b99f2e
abhin012 <abhing03@gmail.com>
2026-06-06 16:30:45 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,11 @@ app.use(express.json());
 
 app.use("/api/resume", resumeRoute);
 
-mongoose.connect("MONGO_URI=mongodb+srv://admin:admin1234@cluster0.rprss.mongodb.net/resumeanalyser?appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log("Server running on port " + process.env.PORT);
+    app.listen(process.env.PORT || 10000, () => {
+      console.log("Server running on port " + (process.env.PORT || 10000));
     });
   })
   .catch((err) => {
