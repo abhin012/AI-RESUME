b0524f998ffb9b629fbb1234c5f3be21ff008c05
abhin012 <abhing03@gmail.com>
2026-06-07 18:10:49 +0530

    Bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import UploadPage from "./pages/UploadPage";
 import ResultPage from "./pages/ResultPage";
-import MatchPage from "./pages/MatchPage";
 import MatchResultPage from "./pages/MatchResultPage";
 
 function App() {
